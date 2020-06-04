4096e501df3279ca20cdee03ba5c106889d646c0
Josh Peterson <petersonjm1@gmail.com>
2020-06-04 11:09:15 +0000

    Add local and merged branches aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
-set -gx PATH ~/bin ~/.cargo/bin $PATH
+set -gx PATH ~/bin ~/.cargo/bin /home/josh/emsdk /home/josh/emsdk/node/12.9.1_64bit/bin /home/josh/emsdk/upstream/emscripten $PATH
 set -gx EDITOR vim
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_newline_cursor yes
