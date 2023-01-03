7427b920d49c31dd4ca71beba7392071537a3a5d
Josh Peterson <joshuap@unity3d.com>
2023-01-02 20:09:49 -0500

    Have multiline prompt in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,8 @@ set -x JEROME_DATABASE_PASSWORD mydata
 set -x VCPKG_FORCE_SYSTEM_BINARIES 1
 set -x EDITOR nvim
 
+set -g theme_newline_cursor yes
+
 # Remove exa for now as it is difficult to install in codespaces
 # alias ls "exa --icons"
 alias bat batcat
