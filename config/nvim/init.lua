d267fed4d6fe279234e2205934c8570b2ce5f9a7
Josh Peterson <josh@modular.com>
2024-11-05 12:44:23 -0500

    Add devicons to the statusline

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -127,6 +127,7 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       options = {
         icons_enabled = false,
