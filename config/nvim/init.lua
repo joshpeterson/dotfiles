76196cc18a4315dd587882efebb76bf770c7a277
Josh Peterson <josh@modular.com>
2024-11-05 12:51:07 -0500

    Remove some comments

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -247,11 +247,9 @@ require('lazy').setup({
 
   },
   'rcarriga/nvim-notify',
-  -- Remove the `use` here if you're using folke/lazy.nvim.
   {
     'Exafunction/codeium.vim',
     config = function()
-      -- Change '<C-g>' here to any keycode you like.
       vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-l>', function() return vim.fn['codeium#CycleCompletions'](1) end,
         { expr = true, silent = true })
