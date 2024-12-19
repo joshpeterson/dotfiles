540670311f652e5e74902dc36239e6876d29831d
Josh Peterson <josh@modular.com>
2024-12-19 13:20:39 -0500

    Use vscode-like keyboard shortcuts for completion

--- config/nvim/lua/config/plugins/blink.lua
+++ config/nvim/lua/config/plugins/blink.lua
@@ -20,7 +20,7 @@ return {
 			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 			-- see the "default configuration" section below for full documentation on how to define
 			-- your own keymap.
-			keymap = { preset = 'default' },
+			keymap = { preset = 'super-tab' },
 
 			appearance = {
 				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
