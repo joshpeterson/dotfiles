2ffae0b017aa1b6698fb844f97f8fd95323a1ce1
Josh Peterson <josh@modular.com>
2025-07-24 15:40:40 -0400

    Remove unused Mojo code formatting via LSP
    Conform.nvim (from the block above in this file) works well.

--- config/nvim/lua/plugins/mojo.lua
+++ config/nvim/lua/plugins/mojo.lua
@@ -56,21 +56,4 @@ return {
       },
     },
   },
-
-  -- Alternative: use LSP formatting if conform is not available
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      -- Format on save using LSP
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.mojo",
-        callback = function()
-          local clients = vim.lsp.get_clients({ bufnr = 0, name = "mojo" })
-          if #clients > 0 and clients[1].supports_method("textDocument/formatting") then
-            vim.lsp.buf.format({ async = false })
-          end
-        end,
-      })
-    end,
-  },
 }
