eaead0148b7a09ce8bd0d288dc6e8b1f4d8b9dc4
Josh Peterson <petersonjm1@gmail.com>
2025-08-07 03:01:30 +0100

    More sane autocomplete configuration

---
+++ config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,25 @@
+-- Configure blink.cmp to not show autocomplete automatically
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      -- Disable automatic popup
+      completion = {
+        trigger = {
+          -- Don't show completion automatically while typing
+          show_on_keyword = false,
+          -- Don't show after accepting a completion
+          show_on_accept_on_trigger_character = false,
+          -- Don't show when entering insert mode
+          show_on_insert_on_trigger_character = false,
+        },
+      },
+      -- Configure keymaps for manual trigger
+      keymap = {
+        preset = "default",
+        -- Use Ctrl+Space to manually trigger completion
+        ["<C-space>"] = { "show", "fallback" },
+      },
+    },
+  },
+}
