f7de443cdf3f3d68fdfa7f8fcfcb65306618e6f4
Josh Peterson <josh@modular.com>
2025-08-15 10:57:49 -0400

    Enable format on save for C++ files

---
+++ config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        cpp = { "clang-format" },
+        c = { "clang-format" },
+        cc = { "clang-format" },
+        cxx = { "clang-format" },
+        h = { "clang-format" },
+        hpp = { "clang-format" },
+      },
+    },
+  },
+}
