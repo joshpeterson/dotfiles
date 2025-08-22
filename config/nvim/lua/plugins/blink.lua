9d3467a4c2bde25f4fa02de2737db42f97764140
Josh Peterson <josh@modular.com>
2025-08-22 08:23:36 -0400

    Improve autocomplete selection with blink

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -13,14 +13,20 @@ return {
           -- Don't show when entering insert mode
           show_on_insert_on_trigger_character = false,
         },
+        list = {
+          -- Auto-select first item when popup opens
+          selection = { preselect = true },
+        },
       },
       -- Configure keymaps for manual trigger
       keymap = {
         preset = "default",
         -- Use Ctrl+Space to manually trigger completion
         ["<C-space>"] = { "show", "fallback" },
+        -- Accept completion with Tab or Enter
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
       },
     },
   },
 }
-
