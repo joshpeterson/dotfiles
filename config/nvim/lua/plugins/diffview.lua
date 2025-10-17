09a41644b4aebf7c178f50dd3b2386129423d5a6
Josh Peterson <josh@modular.com>
2025-10-17 10:54:18 -0400

    Use diffview in neovim as the default diff and merge tools

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -5,4 +5,13 @@ return {
     { "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
     { "<leader>df", "<cmd>DiffviewFileHistory<cr>", desc = "File History Diff" },
   },
+  opts = {
+    view = {
+      merge_tool = {
+        layout = "diff4_mixed",
+        disable_diagnostics = true,
+        winbar_info = true,
+      },
+    },
+  },
 }
