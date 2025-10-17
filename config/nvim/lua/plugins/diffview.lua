323127146443e0174619d9681a2462002ecc8156
Josh Peterson <josh@modular.com>
2025-10-17 10:40:44 -0400

    Use diffview keybindings I can remember

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -2,7 +2,7 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
-    { "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "File History" },
+    { "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+    { "<leader>df", "<cmd>DiffviewFileHistory<cr>", desc = "File History Diff" },
   },
 }
