d05110e108fc889bb31d62e7168042fe1b6ee275
Josh Peterson <petersonjm1@gmail.com>
2025-12-20 07:30:45 -0500

    Update neovim and lazy versions

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,7 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "solargraph",
