6324cf95b9d4010c1ceaec08b46753d09f46f04e
Josh Peterson <petersonjm1@gmail.com>
2026-01-17 07:29:44 -0500

    Add tailwind and disable ruby LSPs for .html.erb

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,23 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        solargraph = {},
-      },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "solargraph",
-      })
-    end,
-  },
-}
+ return {
+   {
+     "neovim/nvim-lspconfig",
+     opts = {
+       servers = {
+         solargraph = {},
+         tailwindcss = {},
+         ruby_lsp = {
+           filetypes = { "ruby" },
+         },
+       },
+     },
+   },
+   {
+     "mason-org/mason.nvim",
+     opts = function(_, opts)
+       vim.list_extend(opts.ensure_installed, {
+         "solargraph",
+         "tailwindcss-language-server",
+       })
+     end,
+   },
+ }
