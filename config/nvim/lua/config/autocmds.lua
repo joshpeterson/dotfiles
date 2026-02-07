6da725a4ca49ec6764bb82482e8f66aef0b657fa
Josh Peterson <petersonjm1@gmail.com>
2026-02-07 11:44:44 -0500

    More markdown formatting

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -11,7 +11,13 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "markdown",
   callback = function()
     vim.opt_local.textwidth = 80
-    vim.opt_local.wrap = true
-    vim.opt_local.linebreak = true
+    vim.opt_local.formatoptions = "t"
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.md",
+  callback = function()
+    vim.cmd("silent! gqG")
   end,
 })
---
+++ config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
+}
