610add401a0bddea81d2f08db22f2751cb3a4b85
Josh Peterson <petersonjm1@gmail.com>
2026-02-28 14:55:13 -0500

    Add diffview against main

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -4,6 +4,7 @@ return {
     keys = {
       { "<leader>dv", function() vim.cmd("DiffviewOpen") end, desc = "Open Diffview" },
       { "<leader>dc", function() vim.cmd("DiffviewClose") end, desc = "Close Diffview" },
+      { "<leader>dm", function() vim.cmd("DiffviewOpen main") end, desc = "Open Diffview vs main" },
     },
   },
 }
